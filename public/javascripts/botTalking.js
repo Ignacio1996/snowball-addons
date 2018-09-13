a1b47845348687b47ac7de72e440a95d8d78472d
Ignacio <in-aguirre@hotmail.com>
2018-09-13 15:46:14 -0400

    addded functionality for showing up!

--- public/javascripts/botTalking.js
+++ public/javascripts/botTalking.js
@@ -1,7 +1,25 @@
+var interactionNumber = 0;
+
+
 function displayAnswers(button) {
     var nextElement = button.parentNode.nextElementSibling.children;
     Object.keys(nextElement).forEach((key) => {
         nextElement[key].style.display = "block";
-        console.log(nextElement[key].style.display);
+        
     });
+    interactionNumber++;
+    var parent = button.parentNode.parentNode;
+    var interactions = document.getElementsByClassName(parent.className);
+    interactions[interactionNumber].style.display = "block";
+    button.disabled = true;
+    
 }
+
+
+
+//Displays first element of array of interactions
+function displayFirstElement(){
+    var interactions = document.getElementsByClassName('interaction');
+    interactions[0].style.display = "";
+}
+
