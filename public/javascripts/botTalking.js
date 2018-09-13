0c361a6bd59124e6b1bc20fe769240fe4f29e59f
Ignacio <in-aguirre@hotmail.com>
2018-09-13 16:12:40 -0400

    It's almost working!!!!

--- public/javascripts/botTalking.js
+++ public/javascripts/botTalking.js
@@ -1,25 +1,37 @@
 var interactionNumber = 0;
 
 
+// Button pressed displays answers
+// Also shows the next interaction
+// Button gets disabled after pressing
+// Both buttons should be disabled
 function displayAnswers(button) {
     var nextElement = button.parentNode.nextElementSibling.children;
     Object.keys(nextElement).forEach((key) => {
         nextElement[key].style.display = "block";
-        
+
     });
     interactionNumber++;
     var parent = button.parentNode.parentNode;
     var interactions = document.getElementsByClassName(parent.className);
-    interactions[interactionNumber].style.display = "block";
+    // Displays next interaction
     button.disabled = true;
-    
+    setTimeout(() => {
+        interactions[interactionNumber].style.display = "block";
+    }, 600)
+
+
 }
 
 
 
 //Displays first element of array of interactions
-function displayFirstElement(){
+function displayFirstElement() {
     var interactions = document.getElementsByClassName('interaction');
     interactions[0].style.display = "";
 }
 
+
+function displayBotAnimation() {
+
+}
