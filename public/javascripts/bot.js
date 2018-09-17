a282315817ad99d242142c1d7c490be5aa0e47a0
Ignacio <in-aguirre@hotmail.com>
2018-09-17 10:57:07 -0400

    form working, needs UI improvements

--- public/javascripts/bot.js
+++ public/javascripts/bot.js
@@ -5,40 +5,39 @@ function addBotForm() {
     var newBotForm = `
     <div class="botForm">
     <div class="botTalks">
-        <img src="/images/bot.png" >
-        <input  class="form-control  class="form-control botTalks${formNum}" style="display: block" type="text" value="Howdy! Im the bot${formNum}" name="bot[talk][${formNum}]" required>
-        <button class="btn btn-primary" type="button" class="btn btn-primary" onclick="addTextClone(this)">V</button>
-    </div>
+    <img class="bot-img" src="/images/bot.png" >
+        <input  class="form-control botTalks${formNum}" type="text" value="Howdy! Im the bot${formNum}" name="bot[talk][${formNum}]" required>
+        <button class="btn btn-primary" type="button" class="btn btn-primary" onclick="addTextClone(this)">+</button>
+        </div>
     
-    <br>
+        <br>
 
     <div class="userForm">
-        <h5 class="student-input">Student Button 1</h5>
         <div class="student">
-            <input  class="form-control student-input"style="display: block" type="text" value="Yes!"name="bot[student][decision][${formNum}]">
-            <img class="student-input" style="margin: 0px" src="/images/user.png" >
+            <img class="student-img" src="/images/user.png" >
+            <input  class="form-control student-input" style="display: block" type="text" value="Yes!"name="bot[student][decision][${formNum}]">
         </div>
         <br>
         <div class="botAnswers">
-            <h5>Bot Answers ${formNum+1}</h5>
+            <img class="bot-img" src="/images/bot.png" >
             <input  class="form-control"style="display: block" type="text" value="Great! Lets move forward!${formNum+1}"  name="bot[student][answer][${formNum}][0]">
-            <button class="btn btn-primary" type="button" onclick="addTextClone(this)">Bot Answer +</button>
+            <button class="btn btn-primary" type="button" onclick="addTextClone(this)">+</button>
         </div>
         <br>
-        <button class="btn btn-primary" type="button" onclick="addStudentForm(this)">Student Decision +</button>
+        <button class="btn btn-primary ml-auto" type="button" onclick="addStudentForm(this)">Student Decision +</button>
     
     </div>
     <br>
     </div>`;
     var currentForm = document.getElementsByClassName('botForm')[formNum ];
-
     currentForm.insertAdjacentHTML('afterend', newBotForm);
 
 }
 
 function addTextClone(element) {    
     var newBotText = element.previousElementSibling.cloneNode(true);
-    element.parentNode.appendChild(newBotText);
+    newBotText.style.display = "block";
+    element.parentNode.insertBefore(newBotText, element.nextSibling);
 }
 
 
@@ -52,14 +51,16 @@ function addStudentForm(element){
 
     var newStudentForm =`
     <div class="userForm">
-        <div class="userFormCount${formNum}"
-            <h5>Student Button 1</h5>
-            <input class="form-control" style="display: block" type="text" value="No" name="bot[student][decision][${formNum}]">
+        <div class="userFormCount${formNum} student"
+            <div class="student">
+            <img class="student-img" src="/images/user.png" >
+            <input class="form-control student-input" type="text" value="No" name="bot[student][decision][${formNum}]">
+            </div>
             <br>
             <div class="botAnswers">
-                <h5>Bot Answers 1</h5>
+                <img src="/images/bot.png" >
                 <input class="form-control" style="display: block" type="text" value="So sad! We will continue anyways"  name="bot[student][answer][${formNum}][${formCount.length +1}]">
-                <button class="btn btn-primary" type="button" onclick="addTextClone(this)">Bot Answer +</button>
+                <button class="btn btn-primary" type="button" onclick="addTextClone(this)">+</button>
             </div>
             <br>
             <button class="btn btn-primary" type="button" onclick="addStudentForm(this)">Student Decision +</button>
